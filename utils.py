5145626a79395339efef511922884a37fbf9a68b
Carter Yagemann <yagemann@gatech.edu>
2018-02-24 14:11:14 +0000

    Manually parse PATH to find binaries because Python subprocesses isn't as reliable as it should be.

--- utils.py
+++ utils.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
 
 import logger
-import subprocess
+import os
+
+module_name = 'Utils'
 
 def lookup_bin(name):
-    p = subprocess.Popen(['which', name], stdout=subprocess.PIPE)
-    return p.stdout.read().strip()
+    """ Looks up the path to a bin using Linux environment variables.
+
+    Not as robust as a program like which, but should be good enough.
+    """
+    logger.log_debug(module_name, 'PATH = ' + str(os.environ['PATH']))
+    path_dirs = os.environ['PATH'].split(':')
+    for path_dir in path_dirs:
+        candidate = os.path.join(path_dir, name)
+        if os.path.isfile(candidate):
+            return candidate
+    logger.log_warning(module_name, 'Failed to find ' + str(name))
+    return '' # Failed to find a match
